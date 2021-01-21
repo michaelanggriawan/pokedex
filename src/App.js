102d1e9179cd7c7f9091e41d7a0ed1b5798909ef
michael anggriawan <michaelanggriawan941@gmail.com>
2021-01-21 12:00:34 +0700

    fix reredner

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         <Container>
           <Header />
           <Switch>
-            <Route exact path="/" render={() => <PokemonListPage />} />
+            <Route exact path="/" component={PokemonListPage} />
             <Route path="/pokemon-detail/:name" component={PokemonDetailPage} />
             <Route path="/my-pokemon" component={MyPokemonDetailPage} />
           </Switch>
